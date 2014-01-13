6819ee24daa3c29448f6703e14e8931360dc9a5e
Vivian Kam <otherviv@gmail.com>
2014-01-12 17:30:45 -0800

    Boom.

---
+++ uber-util.js
@@ -0,0 +1,24 @@
+var util  = require('util'),
+	merge = require('deepmerge');
+
+module.exports = util;
+util.merge = merge;
+
+util.removeEmptyProperties = function (thing) {
+	if (util.isArray(thing)) {
+		thing.forEach(function (item) {
+			util.removeEmptyProperties(item);
+		});
+	} else if (typeof thing === 'object') {
+		for (var property in thing) {
+			if (thing[property] == null || (util.isArray(thing[property]) && thing[property].length === 0)) {
+				delete thing[property];
+			} else if (typeof thing[property] === 'object') {
+				util.removeEmptyProperties(thing[property]);
+				if (Object.keys(thing[property]).length === 0) {
+					delete thing[property];
+				}
+			}
+		}
+	}
+};
